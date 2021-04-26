03deff977b1946a4f447b627788f1acfd380144d
yymin1022 <yymin1022@gmail.com>
2021-04-26 19:28:38 +0900

    Update main.py : Improved POST Structure

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method =='POST':
-        value = request.form['fileURL']
-        return value
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    value = request.form['fileURL']
+    return value
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)

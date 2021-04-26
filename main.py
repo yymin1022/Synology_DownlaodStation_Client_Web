97fb636f7988ceb40d48d559bfc3204638ca8aea
yymin1022 <yymin1022@gmail.com>
2021-04-26 19:03:13 +0900

    Update Files : Send Post Action on Button Clicked

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/register', methods=['POST'])
+def register():
+    value = request.form['fileURL']
+    return value
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
